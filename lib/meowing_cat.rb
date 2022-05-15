ac3a9b452fa7b6d4bf55a60f5af67f1da1d60f7c
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-05-15 16:21:24 -0400

    completed assignment

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,15 @@
 ## code your solution here. 
+require "pry"
+class Cat
+  attr_accessor :name
+  attr_reader :meow
+
+  def name=(name)
+    @name = name
+  end
+
+  def meow 
+    puts "meow!"  
+  end
+
+end 
